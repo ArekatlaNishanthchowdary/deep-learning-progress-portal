16e4febddb3cf1b66951247555bc566669770e90
Arekatla Nishanth Chowdary <av.sc.u4aie23002@av.students.amrita.edu>
2025-07-04 15:53:39 +0530

    trying fix for messengers

--- pages/messenger.py
+++ pages/messenger.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from db.database import (
     get_group_messages, add_group_message, edit_group_message, delete_group_message,
