7ce18800a0a6426a78f8610d590945267a811a96
Arekatla Nishanth Chowdary <av.sc.u4aie23002@av.students.amrita.edu>
2025-07-04 15:49:02 +0530

    fixing the messenger for cloud

--- pages/messenger.py
+++ pages/messenger.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from db.database import (
     get_group_messages, add_group_message, edit_group_message, delete_group_message,
